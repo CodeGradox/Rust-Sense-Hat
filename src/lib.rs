c28604558e1312dd9f24a9f46b0889fe90b8656a
Magnus Åsrud <magnusasrud92@gmail.com>
2016-11-14 22:58:18 +0100

    Now writes to the frame buffer directly

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,6 @@ use color::Color;
 
 pub struct LedDisplay {
     framebuffer: Framebuffer,
-    frame: [u8; 128],
 }
 
 impl LedDisplay {
@@ -41,51 +40,53 @@ impl LedDisplay {
             .next();
 
         match framebuffer {
-            Some(fb) => {
-                Ok(Self {
-                    framebuffer: fb,
-                    frame: [0; 128],
-                })
-            }
+            Some(fb) => Ok(Self { framebuffer: fb }),
             None => {
                 Err(LedDisplayError::new(LedDisplayErrorKind::IoError,
                                          "Cannot detect RPi-Sense FB device"))
             }
         }
-
     }
 
     // Paints the whole LED with a signle color
-    pub fn draw_pixels(&mut self, color: Color) {
+    pub fn set_frame(&mut self, color: Color) {
         let (msb, lsb) = color.split();
+        let mut frame = self.get_frame();
         for i in (0..64).map(|x| x * 2) {
-            self.frame[i] = lsb;
-            self.frame[i + 1] = msb;
+            frame[i] = lsb;
+            frame[i + 1] = msb;
         }
-        self.framebuffer.write_frame(&self.frame);
     }
 
-
     // Draws one pixel on the LED display
-    pub fn draw_pixel(&mut self, x: usize, y: usize, color: Color) {
+    pub fn set_pixel(&mut self, x: usize, y: usize, color: Color) {
         assert!(x <= 7, "X position must be within 0 and 7");
         assert!(y <= 7, "Y position must be within 0 and 7");
 
         let (msb, lsb) = color.split();
         let pos = 2 * (x + y * 8);
-
+        let mut frame = self.get_frame();
         // Each pixel is stored in little endian, so we need to flip
         // the two values as they are big endian.
-        self.frame[pos] = lsb;
-        self.frame[pos + 1] = msb;
-        self.framebuffer.write_frame(&self.frame);
+        frame[pos] = lsb;
+        frame[pos + 1] = msb;
+    }
+
+    pub fn set_pixels(&mut self, color_list: &[Color]) {
+        let mut frame = self.get_frame();
+        for (i, c) in (0..64).map(|x| x * 2).zip(color_list.iter()) {
+            let (msb, lsb) = c.split();
+            frame[i] = lsb;
+            frame[i + 1] = msb;
+        }
     }
 
     pub fn clear(&mut self) {
-        for val in self.frame.iter_mut() {
-            *val = 0;
-        }
-        self.framebuffer.write_frame(&self.frame);
+        self.framebuffer.write_frame(&[0u8; 128]);
+    }
+
+    fn get_frame(&mut self) -> &mut [u8] {
+        return unsafe { self.framebuffer.frame.as_mut_slice() };
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,50 @@
 extern crate rust_sense_hat;
 
 use rust_sense_hat::LedDisplay;
-use rust_sense_hat::color::Color;
+use rust_sense_hat::color::Color::{Hex888, Hex565, Rgb};
 
 fn main() {
-    // use Color::{Rgb, Hex565, Hex888};
     let mut display = LedDisplay::new().unwrap();
     display.clear();
 
-    display.draw_pixel(0, 0, Color::Hex888(0xafcfdb));
-    display.draw_pixel(0, 2, Color::Hex565(0xae7b));
-    display.draw_pixel(0, 4, Color::Rgb(175, 207, 219));
+    display.set_pixel(0, 0, Hex888(0xafcfdb));
+    display.set_pixel(0, 2, Hex565(0xae7b));
+    display.set_pixel(0, 4, Rgb(175, 207, 219));
     display.clear();
-    // display.draw_pixels(Color::Rgb(255, 255, 255));
+    draw_ok(&mut display);
+    draw_bluetooth(&mut display);
+    display.clear();
+    // display.set_frame(Color::Rgb(255, 255, 255));
+}
+
+fn draw_ok(display: &mut LedDisplay) {
+    let g = Rgb(0, 255, 0);
+    let b = Rgb(0, 0, 0);
+    let frame = &[
+        g, g, g, b, g, b, b, g,
+        g, b, g, b, g, b, g, b,
+        g, b, g, b, g, g, b, b,
+        g, b, g, b, g, b, g, b,
+        g, g, g, b, g, b, b, g,
+        b, b, b, b, b, b, b, b,
+        b, b, b, b, b, b, b, b,
+        b, b, b, b, b, b, b, b
+    ];
+    display.set_pixels(frame);
+}
+
+fn draw_bluetooth(display: &mut LedDisplay) {
+    let c = Hex565(0xFFFF);
+    let b = Rgb(0, 0, 255);
+    let frame = &[
+        b, b, b, b, b, c, c, c,
+        c, b, c, c, b, c, c, c,
+        c, c, b, c, b, c, c, c,
+        c, c, c, b, b, b, b, b,
+        c, b, b, b, b, c, c, b,
+        c, c, c, b, c, b, c, b,
+        c, c, c, b, c, c, b, b,
+        c, c, c, c, c, c, c, b
+    ];
+    display.set_pixels(frame);
 }
